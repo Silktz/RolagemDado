f52773f9233ff737d97793862ee7b20e2ed7be07
Silktz <caua.franco3@etec.sp.gov.br>
2023-05-12 20:29:28 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿//Made by Silktz
+
+int facedado; 
+Random prizedraw = new Random (); 
+
+  Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine ("Rolagem de Dado");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Digite a quantidade de faces do dado: ");
+Console.ResetColor();
+
+facedado = int.Parse(Console.ReadLine()!);
+
+if (facedado < 0)
+{
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine("Digite um número de faces validas: ");
+    }
+    else 
+    {
+       DefaultColor();
+
+        int rolls = prizedraw.Next(1, facedado);
+        Console.WriteLine("--- Dado rolando... ---");
+        Console.WriteLine();
+
+        DefaultColor();
+
+        Console.WriteLine($"Número de faces: {facedado}");
+        Console.WriteLine($"Rolagem: {rolls}");
+    }
+
+    void DefaultColor()
+    {
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+    }
